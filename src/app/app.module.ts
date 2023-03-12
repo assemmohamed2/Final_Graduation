c5d6a85bfcb69012e09a2b3a1d0281b3171b9d68
ahmed <ahmedelsaiedd737@gmail.com>
2023-03-12 15:11:24 +0200

    add new component moveList by Ahmed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Authantication/login/login.component';
 import { HeaderComponent } from './Components/dashboard/header/header.component';
 import { CarncyPipe } from './Pipes/carncy.pipe';
 import { DatePipe } from './Pipes/date.pipe';
+import { MovieListComponent } from './Components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { DatePipe } from './Pipes/date.pipe';
     LoginComponent,
     HeaderComponent,
     CarncyPipe,
-    DatePipe
+    DatePipe,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
